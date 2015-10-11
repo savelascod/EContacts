da97c0aaef093e75d0f403a853add098cb24147a
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-11 12:52:28 -0500

    -Added button's events

--- app/src/main/java/com/application/econtacts/app/MainActivity.java
+++ app/src/main/java/com/application/econtacts/app/MainActivity.java
@@ -1,9 +1,10 @@
 package com.application.econtacts.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -35,5 +36,17 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createCompany(View view) {
+    }
+
+    public void updateCompany(View view) {
+    }
+
+    public void retrieveCompany(View view) {
+    }
+
+    public void deleteCompany(View view) {
     }
 }
